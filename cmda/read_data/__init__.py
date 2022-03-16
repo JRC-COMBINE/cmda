b95d19bb35b1c07454af574df0bc8e3a99292e77
pejmanfa <farhadi@combine.rwth-aachen.de>
2022-03-16 09:36:06 +0100

    DataFrameRollingWindow is removed

--- cmda/read_data/__init__.py
+++ cmda/read_data/__init__.py
@@ -1,11 +1,10 @@
 from .import_wfdb import ReadWFDB, RollingWindowWFDB
-from .import_local import ReadCSV, ReadFeather, RollingWindowCSV, RollingWindowDataFrame
+from .import_local import ReadCSV, ReadFeather, RollingWindowCSV
 
 __all__ = [
     "ReadWFDB",
     "ReadCSV",
     "ReadFeather",
     "RollingWindowWFDB",
-    "RollingWindowCSV",
-    "RollingWindowDataFrame"
+    "RollingWindowCSV"
 ]
